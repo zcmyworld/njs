b3a80acccc07b4e071f24a04ddd1f54c697b6107
itgo <itgo@example.com>
2018-03-29 14:28:09 +0800

    执行js

--- njs/index.js
+++ njs/index.js
@@ -1,22 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = function (req, res, next) {
+	let njs = {};
 	res.render = function (templateDir, args) {
 		let templateData = fs.readFileSync(path.join('./', templateDir), 'utf8');
-		let data = getNjsTag(templateData, args)
+		for (let key in args) {
+			njs[key] = args[key];
+		}
+		let data = parseNjsValue(templateData, args)
+		data = parseNjsScript(data, args)
 		res.send(data);
 	}
-	next();
-}
 
-function getNjsTag(data, args) {
-	let njs_value_reg = /<njs\svalue="(.*)".*\/>/g
-	let rs = data.replace(njs_value_reg, function (a, b, c, d) {
-		// console.log(a)//匹配的文本
-		// console.log(b)//对应的变量
-		// console.log(c)//位置
-		let njsValue = b;
-		return args[njsValue]
-	});
-	return rs;
+	function parseNjsValue(data, args) {
+		let njs_value_reg = /<njs\svalue="(.*)".*\/>/g
+		let rs = data.replace(njs_value_reg, function (a, b, c, d) {
+			// console.log(a)//匹配的文本
+			// console.log(b)//对应的变量
+			// console.log(c)//位置
+			let njsValue = b;
+			return args[njsValue]
+		});
+		return rs;
+	}
+
+	function parseNjsScript(data, args) {
+		let reg = /<script\snjs="true">([\s\S]*?)<\/script>/g
+		let rs = data.replace(reg, function (a, b, c, d) {
+			let outputHtml = "";
+			function nhtml(htmlStr) {
+				outputHtml += htmlStr;
+			}
+			let script = b;
+			eval(script)
+			return outputHtml;
+		});
+		return rs;
+
+	}
+	next();
 }
--- reg.js
+++ reg.js
@@ -1,4 +1,9 @@
 
-let reg = /<njs\svalue=".*"\/>/
-data = 'sadf<njs value="saf"/>asdfa'
+// let reg = /<njs\svalue=".*"\/>/
+// data = 'sadf<njs value="saf"/>asdfa'
+// console.log(data.match(reg))
+let reg = /<script\snjs="true">([\s\S]*)<\/script>/
+
+let data = '<script njs="true">he\nlloworld</script>'
+console.log(data)
 console.log(data.match(reg))
